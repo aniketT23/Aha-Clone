586b0cbe4934065c5b59568fb0b913656348dae2
Priya31g <88648418+Priya31g@users.noreply.github.com>
2021-12-14 23:18:42 +0530

    committing changes

--- watchPage/watch-page/src/Components/WatchPage.jsx
+++ watchPage/watch-page/src/Components/WatchPage.jsx
@@ -15,27 +15,31 @@ export const WatchPage =()=>{
        data_getting()
         
      },[])
+     const main_bg = data.main_bg;
+   
     return (
         <>
-            <div className="main_div_p">
+            <div className="main_div_p" style={{backgroungImage:"colorphoto poster without logo final.png"}}>
                 <div className="info_cont_p">
                 <div>
-                    {data.premium&&   <div className="tag_div_p"></div>}
+                    {data.premium&&   <div className="tag_div_p">
+                        <img  src={data.tags} alt="tags" /></div>}
              
                 <div className="title_p"  >
+                <img src={data.title} alt="metaData"/>
                 </div>
                 <div className="metaData_p">
-                    <img src={"data.metaData"} alt="metaData"/>
+                    <img src={data.metaData} alt="metaData"/>
                 </div>
                 <div className="description_title_p">
                {data.description}
                 </div>
                 <div className="button_box_1P">
-                    <button className="triler_button_p">
+                    <button className="triler_button_p" onClick={()=>{console.log("clicked")}}>
                         Play Trailer
                     </button>
-                    <button className="strt_p">
-                     
+                    <button className="strt_p"  >
+                    
                     </button>
                 </div>
                 </div>
