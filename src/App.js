9ffd83f44fadb3608256bf99fc409b600a0ae355
AbhishekRanjan8051 <abhishekranjan8051@gmail.com>
2021-12-19 13:29:36 +0530

    signin page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Player from "./components/Player";
+import Login from "./components/SignIn";
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <Footer />
       <br />
       <Player url="https://www.youtube.com/watch?v=0867JeIfyLY" />
+      <br/>
+      <Login/>
     </div>
   );
 }
